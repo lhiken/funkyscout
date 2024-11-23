e71f208fbb0a9278ddb88cc586b0048826d9a6cd
Chen <lauchenxia@gmail.com>
2024-11-23 00:19:53 -0800

    vercel deployment hotfix

--- client/src/lib/supabase/supabase.ts
+++ client/src/lib/supabase/supabase.ts
@@ -5,8 +5,8 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient<Database>(
-  supabaseUrl,
-  supabaseKey,
+   supabaseUrl,
+   supabaseKey,
 );
 
 export default supabase;
--- client/src/pages/main/auth/auth.tsx
+++ client/src/pages/main/auth/auth.tsx
@@ -5,62 +5,62 @@ import SignupPage from "./sign-up";
 import { Tooltip } from "react-tooltip";
 
 enum AuthMode {
-  SignIn = "signin",
-  SignUp = "signup",
+   SignIn = "signin",
+   SignUp = "signup",
 }
 
 function AuthPage() {
-  const [authMode, setAuthMode] = useState<AuthMode>(AuthMode.SignIn);
+   const [authMode, setAuthMode] = useState<AuthMode>(AuthMode.SignIn);
 
-  return (
-    <>
-      <div className={styles.authRoot}>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <div>
-              <i className="fa-solid fa-binoculars" />
-              &nbsp; Funkyscout
+   return (
+      <>
+         <div className={styles.authRoot}>
+            <div className={styles.container}>
+               <div className={styles.header}>
+                  <div>
+                     <i className="fa-solid fa-binoculars" />
+                     &nbsp; Funkyscout
+                  </div>
+                  <div
+                     className="theme-selector"
+                     style={{
+                        color: "var(--text-secondary)",
+                        fontSize: "1.05rem",
+                     }}
+                  >
+                     <i className="fa-solid fa-circle-half-stroke"></i>
+                     <Tooltip anchorSelect=".theme-selector" place="top">
+                        Switch theme
+                     </Tooltip>
+                  </div>
+               </div>
+               <div className={styles.modebox}>
+                  <button
+                     className={`${styles.modeButton} ${styles.signinButton} ${
+                        styles[
+                           authMode == AuthMode.SignIn ? "active" : "inactive"
+                        ]
+                     }`}
+                     onClick={() => setAuthMode(AuthMode.SignIn)}
+                  >
+                     Sign in
+                  </button>
+                  <button
+                     className={`${styles.modeButton} ${styles.signupButton} ${
+                        styles[
+                           authMode == AuthMode.SignUp ? "active" : "inactive"
+                        ]
+                     }`}
+                     onClick={() => setAuthMode(AuthMode.SignUp)}
+                  >
+                     Sign up
+                  </button>
+               </div>
+               {authMode == AuthMode.SignIn ? <SigninPage /> : <SignupPage />}
             </div>
-            <div
-              className="theme-selector"
-              style={{
-                color: "var(--text-secondary)",
-                fontSize: "1.05rem",
-              }}
-            >
-              <i className="fa-solid fa-circle-half-stroke"></i>
-              <Tooltip anchorSelect=".theme-selector" place="top">
-                Switch theme
-              </Tooltip>
-            </div>
-          </div>
-          <div className={styles.modebox}>
-            <button
-              className={`${styles.modeButton} ${styles.signinButton} ${
-                styles[
-                  authMode == AuthMode.SignIn ? "active" : "inactive"
-                ]
-              }`}
-              onClick={() => setAuthMode(AuthMode.SignIn)}
-            >
-              Sign in
-            </button>
-            <button
-              className={`${styles.modeButton} ${styles.signupButton} ${
-                styles[
-                  authMode == AuthMode.SignUp ? "active" : "inactive"
-                ]
-              }`}
-              onClick={() => setAuthMode(AuthMode.SignUp)}
-            >
-              Sign up
-            </button>
-          </div>
-          {authMode == AuthMode.SignIn ? <SigninPage /> : <SignupPage />}
-        </div>
-      </div>
-    </>
-  );
+         </div>
+      </>
+   );
 }
 
 export default AuthPage;
