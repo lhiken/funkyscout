050019f95c830dc1a5d8d02bcae3e88426dc3ae3
ZyteByte <lauchenxia@gmail.com>
2024-12-22 19:07:06 -0800

    picklist page progress

--- client/src/pages/desktop/picklists/picklist-tab/picklist-tab.tsx
+++ client/src/pages/desktop/picklists/picklist-tab/picklist-tab.tsx
@@ -1,15 +1,56 @@
+import { useContext, useState } from "react";
 import styles from "./picklist-tab.module.css";
+import { PicklistDataContext } from "../picklists-context";
+import RoundInput from "../../../../components/app/input/round-input";
+import { Picklist } from "../../../../schemas/schema";
+import { Tables } from "../../../../lib/supabase/database.types";
 
 function PicklistTab() {
    return (
       <div className={styles.container}>
          <div className={styles.containerHeader}>
             <div>
-               <i className="fa-solid fa-list-ul" />&nbsp; Picklist
+               <i className="fa-solid fa-list-ul" />&nbsp; Picklists
             </div>
+         </div>
+         <div className={styles.contentContainer}>
+            <PicklistSelectionTab />
          </div>
       </div>
    );
+}
+
+function PicklistSelectionTab() {
+   const [picklistQuery, setPicklistQuery] = useState("");
+   const picklistData = useContext(PicklistDataContext);
+
+   return (
+      <div className={styles.selectionContent}>
+         <RoundInput value={picklistQuery} style={{ borderColor: "var(--text-background)" }} setValue={setPicklistQuery} placeholder="Search picklists..." type="text" cornerStyle="sharp" icon={
+            <i className="fa-solid fa-magnifying-glass" />
+         } />
+         <div className={styles.picklistList}>
+            {
+               picklistData.val?.picklists.map((val, index) => {
+                  return <PicklistCard picklist={val} key={index}/>
+               }) || ""
+            }
+         </div>
+         <div className={styles.newPicklistButton}>
+            Create a new picklist
+         </div>
+      </div>
+   )
+}
+
+function PicklistCard({
+   picklist
+}: { picklist: Tables<"event_picklist"> }) {
+   return (
+      <div className={styles.PicklistCard}>
+         {picklist.title}
+      </div>
+   )
 }
 
 export default PicklistTab;
